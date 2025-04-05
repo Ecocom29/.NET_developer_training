66bae15fc1f24bbc94927d17e31e4b550238c20e
Eliezer Cocom <Eliezer Cocom@HP-VICTUS-ESCC>
2025-04-05 11:30:49 -0600

    3 - Update with the new basic project

--- Basic Level/StructureProgram_Basic_Level/StructureProgram_Basic_Level/obj/Debug/net8.0/StructureProgram_Basic_Level.AssemblyInfo.cs
+++ Basic Level/StructureProgram_Basic_Level/StructureProgram_Basic_Level/obj/Debug/net8.0/StructureProgram_Basic_Level.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StructureProgram_Basic_Level")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47e5ed1ef99638b1ae61eb5f2de6d0112cfe0cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634c73cff619a7a5a7a060780e81986668bd8aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StructureProgram_Basic_Level")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StructureProgram_Basic_Level")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Basic Level/TypeOfVariables_Basic_Level/TypeOfVariables_Basic_Level/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TypeOfVariables_Basic_Level
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
